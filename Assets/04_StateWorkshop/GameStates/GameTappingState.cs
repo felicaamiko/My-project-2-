294d671383d74df03c62b5404d38f9ab902fdecb
clement lee <felicxaamiko@gmail.com>
2024-02-15 02:20:34 -0600

    adding jump

--- Assets/04_StateWorkshop/GameStates/GameTappingState.cs
+++ Assets/04_StateWorkshop/GameStates/GameTappingState.cs
@@ -8,20 +8,20 @@ public class GameTappingState : State
     private GameFSM _stateMachine;
     private GameController _controller;
 
-    int _headCount;
-
+    public float offset;
+    public bool killswitch = false; // extra duct tape
     public GameTappingState(GameFSM stateMachine, GameController controller)
     {
         _stateMachine = stateMachine;
         _controller = controller;
 
-        _headCount = controller.HeadCount;
+        //_headCount = controller.HeadCount;
     }
 
     public override void Enter()
     {
         base.Enter();
-        Debug.Log("use tilt to move around. tap to jump");
+        Debug.Log("use tilt to move around. tap (Q) to jump");
     }
 
     public override void Exit()
@@ -36,36 +36,58 @@ public class GameTappingState : State
 
     public override void Update()
     {
+        
         base.Update();
-        // tap a head with Q - decide if it's a goose!
+        // tap a head with Q - jump
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            DetermineGoose();
+            Jump();
         }
+
+        if (StateDuration > offset + 4)
+        {
+            Land();
+        }
+
+
+
         // if we ever press Spacebar during tap state, we've tried to catch a duck.
         // this is not allowed! Game Over
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            //TODO Change to Lose State instead
-            Debug.Log("You caught a duck. Try again...");
-            _stateMachine.ChangeState(_stateMachine.SetupState);
-        }
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    //TODO Change to Lose State instead
+        //    Debug.Log("You caught a duck. Try again...");
+        //    _stateMachine.ChangeState(_stateMachine.SetupState);
+        //}
     }
 
-    void DetermineGoose()
+    void Jump()
     {
-        // choose a random number out of the total number of heads
-        int gooseDecider = UnityEngine.Random.Range(1, _headCount + 1);
-        if (gooseDecider == 1)
-        {
-            // enter Goose State
-            Debug.Log("GOOSE");
-            _stateMachine.ChangeState(_stateMachine.GooseState);
-        }
-        else
-        {
-            Debug.Log("Duck. Keep tapping!");
-            // remain in Tapping State
-        }
+        offset = StateDuration;
+        //Debug.Log(offset);
+        Debug.Log("jumped");
+
+
+
+
+
+        //// choose a random number out of the total number of heads
+        //int gooseDecider = UnityEngine.Random.Range(1, _headCount + 1);
+        //if (gooseDecider == 1)
+        //{
+        //    // enter Goose State
+        //    Debug.Log("GOOSE");
+        //    _stateMachine.ChangeState(_stateMachine.GooseState);
+        //}
+        //else
+        //{
+        //    Debug.Log("Duck. Keep tapping!");
+        //    // remain in Tapping State
+        //}
+    }
+
+    void Land() {
+        Debug.Log("landed");
+        killswitch = true;
     }
 }
